022e4564994b9cde5da6fef41787bbb379cefdd9
Ben Burlingham <ben.burlingham@gmail.com>
2017-01-09 19:52:55 -0500

    Further work on background tests.

--- background.js
+++ background.js
@@ -1,38 +1,37 @@
-const Background = {
-    isActive: false
-};
-
-module.exports = {
-    onActive: function(tab) {
-        Background.isActive = true;
-
+Background = {
+    setOn: function(tab) {
         chrome.browserAction.setIcon({
             path : {
-                "16": "images/icon16-inactive.png",
-                "48": "images/icon48-inactive.png",
-                "128":  "images/icon128-inactive.png"
+                "16": "images/icon16-off.png",
+                "48": "images/icon48-off.png",
+                "128":  "images/icon128-off.png"
             },
             tabId: tab.id
         });
 
+        chrome.browserAction.setTitle({ title: 'Protractor (active)' });
     },
 
-    onInactive: function(tab) {
-        Background.isActive = false;
-
+    setOff: function(tab, cb) {
         chrome.browserAction.setIcon({
             path: chrome.runtime.getManifest().icons,
             tabId: tab.id
         });
+
+        chrome.browserAction.setTitle({ title: 'Protractor' });
     },
 
     handleClick: function(tab) {
-        Background.isActive
-            ? Background.onInactive(tab)
-            : Background.onActive(tab);
+        chrome.browserAction.getTitle({}, str => {
+            const isOn = (str.indexOf('(active)') > -1);
 
-        chrome.tabs.sendMessage(tab.id, { active: Background.isActive });
+            isOn
+                ? Background.setOff(tab)
+                : Background.setOn(tab);
+
+            chrome.tabs.sendMessage(tab.id, { isOn });
+        });
     },
 };
 
-chrome.browserAction.onClicked.addListener(module.exports.handleClick);
+chrome.browserAction.onClicked.addListener(Background.handleClick);
--- test/background.test.js
+++ test/background.test.js
@@ -1,8 +1,27 @@
 chrome = require('sinon-chrome');
-const test = require('tape');
-const Background = require('../background');
 
-test('click listener added to the browser action', (t) => {
-    t.ok(chrome.browserAction.onClicked._listeners.length > 0)
+const test = require('tape');
+require('../background');
+// const sinon = require('sinon');
+
+const tab = {
+    id: 777
+};
+
+test('init adds click listener to browser action', t => {
+    t.equal(chrome.browserAction.onClicked._listeners.length, 1);
+    t.end();
+});
+
+test('turning on changes icons and title', t => {
+    // console.warn(chrome)
+    // const spy = sinon.spy(chrome.browserAction.setIcon)
+    // Background.setOn(tab);
+    // console.warn(spy.called)
+    t.end();
+});
+
+test('turning off changes icons and title', t => {
+
     t.end();
 });
