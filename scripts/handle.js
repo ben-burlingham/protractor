bf07e98937f59d383ba05bc9a2e33a8d66a0c2d9
Ben Burlingham <ben.burlingham@gmail.com>
2017-01-15 19:18:38 -0800

    Resize function rework, finalized.

--- scripts/handle.js
+++ scripts/handle.js
@@ -1,17 +1,19 @@
-Protractor.Handle = function({ appId, container, i }) {
-    Object.assign(this, { appId, container });
+Handle = function({ appId, i }) {
+    Object.assign(this, { appId });
 
     const ref = this.move.bind(this);
-    const div = document.createElement('div');
-    div.className = `${appId}-handle ${appId}-handle-${i}`;
 
-    div.addEventListener('mousedown', this.dragstart.bind(null, ref));
+    this.node = document.createElement('div');
+    this.node.className = `${appId}-handle ${appId}-handle-${i}`;
+
+    this.node.addEventListener('mousedown', this.dragstart.bind(null, ref));
     document.body.addEventListener('mouseup', this.dragend.bind(null, ref));
+    document.body.addEventListener('mouseenter', this.dragend.bind(null, ref));
 
-    return div;
+    return this.node;
 };
 
-Protractor.Handle.prototype = {
+Handle.prototype = {
     dragstart: function(ref, evt) {
         evt.stopPropagation();
         evt.preventDefault();
@@ -33,32 +35,12 @@ Protractor.Handle.prototype = {
             offset = Math.min(evt.movementX, evt.movementY);
         } else if (evt.movementX >= 0 && evt.movementY >= 0) {
             offset = Math.max(evt.movementX, evt.movementY);
+        } else if (evt.movementX < 0) {
+            offset = (Math.abs(evt.movementX) < evt.movementY ? evt.movementY : evt.movementX);
+        } else {
+            offset = (Math.abs(evt.movementY) < evt.movementX ? evt.movementX : evt.movementY);
         }
-        // else if (evt.movementX < 0) {
-        //     offset = (Math.abs(evt.movementX) < evt.movementY ? evt.movementY : evt.movementX);
-        // } else {
-        //     offset = (Math.abs(evt.movementY) < evt.movementX ? evt.movementX : evt.movementY);
-        // }
 
-        const conBounds = this.container.getBoundingClientRect();
-        // const docBounds = document.body.getBoundingClientRect();
-        //
-        // if ((conBounds.width - 2 * offset) < 300 || (conBounds.height - 2 * offset) < 300) {
-        //     return;
-        // }
-        //
-        // if ((conBounds.left + offset) < docBounds.left || (conBounds.top + offset) < docBounds.top) {
-        //
-        // }
-
-        this.container.style.left = `${conBounds.left + offset}px`;
-        this.container.style.top = `${conBounds.top + offset}px`;
-
-        this.container.style.width = `${conBounds.width - 2 * offset}px`;
-        this.container.style.height = `${conBounds.height - 2 * offset}px`;
-
-        // TODO pubsub
-        // const circle = document.querySelector('.protractor-circle');
-        // circle.style.borderRadius = `${(conBounds.width - 2 * offset) / 2}px`
+        PubSub.emit(Channels.RESIZE, { offset })
     }
 };
